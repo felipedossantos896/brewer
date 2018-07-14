ba48376e067984d82557246b390de67a20d6fbf9
Felipe dos Santos Carneiro <felipedossantos896@gmail.com>
2018-07-14 18:23:39 -0300

    Implementando CadastroEstilo.html

---
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -0,0 +1,15 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/estilos")
+public class EstiloController {
+
+	@RequestMapping("/novo")
+	public String novo() {
+		return "/estilo/CadastroEstilo";
+	}
+	
+}
